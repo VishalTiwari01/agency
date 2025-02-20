b0a876509c40af08979b3ab489ed050670157a51
vishal3838 <vishal990007@gmail.com>
2025-02-20 17:20:05 +0530

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',         // Custom output directory
+    assetsDir: 'assets',     // Assets will be in build/assets/
+    emptyOutDir: true        // Clear the build folder before building
+  },
   plugins: [react()],
 })
