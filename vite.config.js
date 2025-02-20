9521fff4a97f4d8a3083df45cb6a4e44239ac1b6
vishal3838 <vishal990007@gmail.com>
2025-02-20 17:28:44 +0530

    changed vite.config.js 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',         // Custom output directory
+    outDir: 'dist',         // Custom output directory
     assetsDir: 'assets',     // Assets will be in build/assets/
     emptyOutDir: true        // Clear the build folder before building
   },
